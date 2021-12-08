7c94c0b7f9516d6df84015d8eb01cd99cea3802c
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-12-08 16:55:14 -0600

    addedplayer Component/ class

--- raygame/Component.cpp
+++ raygame/Component.cpp
@@ -1,1 +1,43 @@
 #include "Component.h"
+
+Component::Component()
+{
+}
+
+Component::Component(Actor*, const char*)
+{
+}
+
+Component::~Component()
+{
+}
+
+const char* Component::getName()
+{
+	return nullptr;
+}
+
+Actor* Component::getOwner()
+{
+	return nullptr;
+}
+
+void Component::start()
+{
+}
+
+void Component::update(float)
+{
+}
+
+void Component::draw()
+{
+}
+
+void Component::end()
+{
+}
+
+void Component::onCollision(Actor*)
+{
+}
