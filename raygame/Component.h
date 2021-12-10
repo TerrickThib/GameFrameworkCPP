83e7ae64dbf2ac18f009830c0e0e9c0deaacbde5
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-12-10 11:57:10 -0600

    Added Move Component

--- raygame/Component.h
+++ raygame/Component.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "Actor.h"
-class Component :
-	public Actor
+class Component
 {
 public:
 	Component();
---
+++ raygame/MoveComponent.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Component.h"
+#include <Vector2.h>
+#include <Matrix3.h>
+
+class MoveComponent :
+	public Component
+{
+
+private:
+	MathLibary::Vector2()
+	float m_speed;
+
+public:
+	void start()
+	{
+
+	}
+};
+
