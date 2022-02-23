3eeb94bd7b92e6187bd760d023aecd76a624ea2f
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2022-02-23 14:18:51 -0600

    Transfered DynamicArray

--- raygame/ActorArray.h
+++ raygame/ActorArray.h
@@ -1,50 +1,132 @@
 #pragma once
 class Actor;
 
-class ActorArray
+template <typename T>
+class DynamicArray
 {
 public:
-	/// <summary>
-	/// Adds an actor to the end of the array
-	/// </summary>
-	/// <param name="actor">The new actor to add to the array</param>
-	void addActor(Actor* actor);
+	DynamicArray();
+	~DynamicArray();
 
-	/// <summary>
-	/// Removes the actor from the array
-	/// </summary>
-	/// <param name="actor">The actor to remove</param>
-	/// <returns>False if the actor is not in the array</returns>
-	bool removeActor(Actor* actor);
-	/// <summary>
-	/// Removes an actor from the array
-	/// </summary>
-	/// <param name="index">The index to remove from the array</param>
-	/// <returns>False if the index is out of bounds</returns>
-	bool removeActor(int index);
+	void addItem(T item);
 
-	/// <summary>
-	/// Gets an actor from the array
-	/// </summary>
-	/// <param name="index">The index of the desired actor</param>
-	/// <returns>The actor found at the index or nullptr if the index was out of bounds</returns>
-	Actor* getActor(int index);
+	void addItems(T item[], int size);
 
-	/// <summary>
-	/// Gets the length of the array
-	/// </summary>
-	int getLength() { return m_actorCount; }
+	bool removeItem(T item);
 
-	/// <summary>
-	/// Checks if the actor is in the array
-	/// </summary>
-	/// <param name="actor">A reference to the actor to use to check</param>
-	/// <returns>False if the actor is not in the array</returns>
-	bool contains(Actor* actor);
+	void sortItems();
+
+	T getItem(int index);
+
+	int getLength() { return m_length; }
 
 private:
-	Actor** m_actors;
-	int m_actorCount;
+	T* m_items;
+	int m_length;
 };
 
+template<typename T>
+inline DynamicArray<T>::DynamicArray()
+{
+	m_length = 0;
+	m_items = nullptr;
+}
 
+template<typename T>
+inline DynamicArray<T>::~DynamicArray()
+{
+}
+
+template<typename T>
+inline void DynamicArray<T>::addItem(T item)
+{
+	//Create a new array with a size one greater than our old array
+	T* tempArray = new T[getLength() + 1];
+	//Copy the values from the old array to the new array
+	for (int i = 0; i < getLength(); i++)
+	{
+		tempArray[i] = m_items[i];
+	}
+
+	//Set the last value in the new array to be the actor we want to add
+	tempArray[getLength()] = item;
+
+	delete[] m_items;
+	//Set old array to hold the values of the new array
+	m_items = tempArray;
+	m_length++;
+}
+
+template<typename T>
+inline void DynamicArray<T>::addItems(T item[], int size)
+{
+
+}
+
+template<typename T>
+inline bool DynamicArray<T>::removeItem(T item)
+{
+	//Check to see if the actor was null 
+	if (!item || getLength() <= 0)
+	{
+		return false;
+	}
+
+	bool itemRemoved = false;
+	//Create a new array with a size one less than our old array
+	T* tempArray = new T[getLength() - 1];
+	//Create a variable to access tempArray index  
+	int j = 0;
+	//Copy values from the old array to the new array  
+	for (int i = 0; i < getLength(); i++)
+	{
+		if (item != m_items[i] || itemRemoved)
+		{
+			tempArray[j] = m_items[i];
+			j++;
+		}
+		else
+		{
+			itemRemoved = true;
+		}
+	}
+	//Set the old array to the new array  
+	if (itemRemoved)
+	{
+		delete[] m_items;
+		m_items = tempArray;
+		m_length--;
+	}
+	//Return whether or not the removal was successful  
+	return itemRemoved;
+}
+
+template<typename T>
+inline void DynamicArray<T>::sortItems()
+{
+	T key;
+	int j = 0;
+
+	for (int i = 1; i < getLength(); i++)
+	{
+		key = m_items[i]; //Sets key to current index
+		j = i - 1;//sets j to pervious index
+		while (j >= 0 && m_items[j] > key)//While j is greater than oe equal to o and the value at j is greater than key
+		{
+			m_items[j + 1] = m_items[j];//Set array at the index of j + 1 to be the value at index of j
+			j--;//Decrments
+		}
+
+		m_items[j + 1] = key;//Set array at the index of j+1 to be the key
+	}
+}
+
+template<typename T>
+inline T DynamicArray<T>::getItem(int index)
+{
+	//Return false if the index is out of bounds
+	if (index < 0 || index >= getLength())
+		return T();
+
+	return m_items[index];
+}
